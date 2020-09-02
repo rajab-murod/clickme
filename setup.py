6de33c86a1360da2864308af538c38a2c5781454
rajab-murod <53085103+rajab-murod@users.noreply.github.com>
2020-09-02 15:33:00 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="paymeuz",
-    version="0.1",
+    name="clickme",
+    version="1.0",
     author="rajab-murod",
     author_email="rajab.murod1995@gmail.com",
-    description="paymeuz",
+    description="clickme",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.5",
     install_requires=['requests', 'django', 'djangorestframework'],
-    url="https://github.com/rajab-murod/paymeuz",
+    url="https://github.com/rajab-murod/clickme",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
