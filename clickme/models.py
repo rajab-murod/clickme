50e6c86d063b48f4e8312b7b0be571d03d27d240
rajab-murod <53085103+rajab-murod@users.noreply.github.com>
2020-09-06 14:38:07 +0500

    clickme

--- clickme/models.py
+++ clickme/models.py
@@ -3,30 +3,38 @@ from django.db import models
 
 class Transaction(models.Model):
     PROCESS = 0
-    PAID = 1
-    FAILED = 2
+    VERIFY = 1
+    PAID = 2
+    FAILED = 3
+
     STATUS = (
         (PROCESS, 'processing'),
+        (VERIFY, 'verify'),
         (PAID, 'paid'),
-        (FAILED, 'failed'),
+        (FAILED, 'failed')
     )
 
-    payment_id = models.CharField(max_length=255)
-    request_id = models.IntegerField(null=True, blank=True)
+    payment_id = models.CharField(max_length=255, blank=True, null=True)
+    order_id = models.IntegerField(null=True, blank=True)
     amount = models.DecimalField(decimal_places=2, default=0.00, max_digits=10)
     status = models.CharField(max_length=10, default=PROCESS, choices=STATUS)
+    error_code = models.IntegerField(blank=True, null=True)
+    error_note = models.CharField(max_length=255, blank=True, null=True)
     create_time = models.DateTimeField(auto_now_add=True)
     pay_time = models.DateTimeField(auto_now=True)
 
-    def create_transaction(self, payment_id, request_id, amount, status):
+    def create_transaction(self, error_code, error_note, status=PROCESS):
         Transaction.objects.create(
-            payment_id=payment_id,
-            request_id=request_id,
-            amount=amount / 100,
+            error_code=error_code,
+            error_note=error_note,
             status=status
         )
 
-    def update_transaction(self, payment_id, status):
-        trans = Transaction.objects.get(payment_id=payment_id)
+    def update_transaction(self, trans, status, error_code, error_note, payment_id=None, order_id=None, amount=None):
         trans.status = status
+        trans.error_code = error_code
+        trans.error_note = error_note
+        trans.order_id = order_id
+        trans.amount = amount
+        trans.payment_id = payment_id
         trans.save()
